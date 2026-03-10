a0f607a5a35e2b16b3a4d758c0cb43eee2a6ed7a
kapil212a <amangupta74086@gmail.com>
2026-03-10 23:31:32 +0530

    change

--- Main.java
+++ Main.java
@@ -61,10 +61,12 @@ public class Main {
     //Question -- 4
     public static void Equation(){
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Enter the Value of a : ");
         int a = sc.nextInt();
+        System.out.print("Enter the Value of b : ");
         int b = sc.nextInt();
-        int c = sc.nextInt();  // not used but taken as per question
+        // System.out.print("Enter the Value of c : ");
+        // int c = sc.nextInt();  
 
         int result = (a*a*a) + (a*a*b) + (2*a*a*b) + (2*a*b*b) + (a*b*b) + (b*b*b);
 
@@ -80,8 +82,9 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-
+            System.out.print("Enter The Number Of Cars:");
             int cars = sc.nextInt();
+            System.out.print("Enter The Number Of Bikes:");
             int bikes = sc.nextInt();
 
             int tyres = (cars * 4) + (bikes * 2);
@@ -94,7 +97,7 @@ public class Main {
 
     public static void MinDiscount(){
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Enter The Number of Product :");
         int n = sc.nextInt();
         sc.nextLine();
 
@@ -121,6 +124,94 @@ public class Main {
         System.out.println(itemName);
     }
 
+    //Question -- 7
+    public static void findFactor(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Number : ");
+        int n = sc.nextInt();
+
+        n = Math.abs(n);
+
+        if (n == 0) {
+            System.out.println("No Factors");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                System.out.print(i + " ");
+        }
+    }
+
+    //Question -- 8
+    public static void matrixRotation(int matrix[][]){
+        int n = matrix.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+
+        for(int[] row : matrix){
+            for(int val : row){
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //Question -- 9
+    public static int binarySearch(int [] arr , int target){
+            int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            else if (arr[mid] < target)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+
+    //Question -- 10
+    public static void FrequencyCount(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Size : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            map.put(arr[i], map.getOrDefault(arr[i],0)+1);
+        }
+
+        for(int key : map.keySet()){
+            System.out.println(key + " occurs " + map.get(key) + " times");
+        }
+    }
+
     public static void main(String[] args) {
         int num = 29;
 
@@ -141,6 +232,21 @@ public class Main {
         Tyres();
 
         MinDiscount();
+
+        findFactor();
+
+        int matrix[][] = {
+                    {1,2,3},
+                    {4,5,6},
+                    {7,8,9}
+        };
+        matrixRotation(matrix);
+
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int target = 4;
+        System.out.println(binarySearch(arr, target));
+
+        FrequencyCount();
     
     }
 }

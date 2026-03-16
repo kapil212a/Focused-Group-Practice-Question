6c322370f393215105b057c500098a8b1e0ffcd8
kapil212a <amangupta74086@gmail.com>
2026-03-16 23:37:50 +0530

    change

--- Largest.java
+++ Largest.java
@@ -29,7 +29,25 @@ public class Largest {
         System.out.println("Second Largest : " + secondLargest);
     }
     
-    
+    public static void second(int arr[]){
+        
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } 
+            else if(arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest = " + secondLargest);
+    }    
+
 
     public static void main(String[] args) {
         secondLargest();
